d7761085efa35f03cdc0951a544fb341809b562f
Jonathan Davies <jonathan.davies@jetstack.io>
2017-07-04 14:48:53 +0100

    prometheus_rule: Made order simpler.

--- manifests/prometheus_rule.pp
+++ manifests/prometheus_rule.pp
@@ -1,9 +1,10 @@
 define prometheus::prometheus_rule (
   $alert_name = $title,
-  $alert_if = '',
-  $alert_for = '',
-  $alert_summary = '',
-  $alert_description = '',
+  $alert_if,
+  $alert_for,
+  $alert_summary,
+  $alert_description,
+  $order,
 ) {
   if ! defined(Class['kubernetes::apiserver']) {
     fail('This defined type can only be used on the kubernetes master')
@@ -11,7 +12,7 @@ define prometheus::prometheus_rule (
 
   kubernetes::apply_fragment { "prometheus-rules-${title}":
     content => template('prometheus/prometheus-rule.yaml.erb'),
-    order   => '01',
+    order   => $order,
     target  => '/etc/kubernetes/apply/prometheus-rules.yaml',
   }
 }
