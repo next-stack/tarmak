7fcfe9b1de0abe5a6a58c42d3333bf520ddd3d15
Jonathan Davies <jonathan.davies@jetstack.io>
2017-07-05 14:14:14 +0100

    prometheus_rule_spec: Test for changing alert_label.

--- spec/defines/prometheus_rule_spec.rb
+++ spec/defines/prometheus_rule_spec.rb
@@ -25,4 +25,27 @@ describe 'prometheus::prometheus_rule', :type => :define do
       .with_content(/severity="page"/)
       .with_content(/"{{\$labels.instance}}: High CPU usage detected"/)
   end
+
+  context 'specified alert_label severity' do
+    let :params do
+      {
+        :alert_if          => '(100 - (avg by (instance) (irate(node_cpu{name="node-exporter",mode="idle"}[5m])) * 100)) > 75',
+        :alert_for         => "2m",
+        :alert_summary     => '{{$labels.instance}}: High CPU usage detected',
+        :alert_description => '{{$labels.instance}}: CPU usage is above 75% (current value is: {{ $value }})',
+        :order             => '02',
+        :alert_labels =>
+          {
+            'severity' => 'critical'
+          },
+      }
+    end
+    it do
+      should contain_concat__fragment("kubectl-apply-prometheus-rules-cpu-usage")
+        .with_content(/cpu-usage.rules/)
+        .with_content(/ALERT cpu-usage/)
+        .with_content(/severity="critical"/)
+        .with_content(/"{{\$labels.instance}}: High CPU usage detected"/)
+    end
+  end
 end
