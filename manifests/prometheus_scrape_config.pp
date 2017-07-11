4feb408eadfc9e221b2e0f1330eca4c90624c165
Jonathan Davies <jonathan.davies@jetstack.io>
2017-07-11 12:38:12 +0100

    prometheus_scrape_config: Use config option for hashes.

--- manifests/prometheus_scrape_config.pp
+++ manifests/prometheus_scrape_config.pp
@@ -1,24 +1,13 @@
 define prometheus::prometheus_scrape_config (
+  $config = {},
   $order,
-  $metrics_path = undef,
-  $scheme = undef,
-  $scrape_interval = undef,
-  $scrape_timeout = undef,
-  $basic_auth = {},
-  $bearer_token = undef,
-  $bearer_token_file = undef,
-  $kubernetes_sd_configs = {},
-  $metric_relabel_configs = {},
-  $params = {},
-  $proxy_url = undef,
-  $relabel_configs = {},
-  $static_configs = {},
-  $tls_config = undef,
   $job_name = $title,
 ) {
   if ! defined(Class['kubernetes::apiserver']) {
     fail('This defined type can only be used on the kubernetes master')
   }
+
+  $prepared_config = fragmenthash2yaml($config)
 
   kubernetes::apply_fragment { "prometheus-scrape-config-${job_name}":
     content => template('prometheus/prometheus-config-frag.yaml.erb'),
